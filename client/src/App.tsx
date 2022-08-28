68ea03334504269fb2f22a061d798f5f3092408b
z00mni <chjm219@kookmin.ac.kr>
2022-08-28 14:01:01 +0000

    useDebounce for searching

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,26 @@
 import { ChangeEvent, useCallback, useState } from 'react'
 import { useGetMusicByKeywordQuery } from './features/musicSlice'
+import useDebounce from './hooks/useDebounce'
 
 function App() {
-  const [keyword, setKeyword] = useState("")
-  const { data, error, isLoading } = useGetMusicByKeywordQuery(keyword)
+  const [value, setValue] = useState("")
+  const keyword = useDebounce(value, 500) 
+  const { data, error, isLoading } = useGetMusicByKeywordQuery(keyword, {
+    skip: value.trim().length === 0
+  })
   const onChangeKeyword = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setKeyword(e.target.value)
-  },[keyword, setKeyword])
+    setValue(e.target.value)
+  },[value, setValue])
 
   return (
     <div className="App">
-      <input value={keyword} onChange={onChangeKeyword}/>
+      <input value={value} onChange={onChangeKeyword}/>
       {isLoading && <p>Loading..</p>}
       {data && data.map((d: any, i: number) => (
         <div key={i}>
           <img src={d.album.images[2].url}/>
           <p>{d.name}</p>
+          <p>{d.artists.map((a: any, i: number) => a.name)}</p>
         </div>
       ))}
     </div>
