ae5f872ba741150ac825a23f49013a57c1e46e4b
z00mni <chjm219@kookmin.ac.kr>
2022-09-10 14:05:19 +0000

    nav icons added

--- client/src/components/common/Nav.tsx
+++ client/src/components/common/Nav.tsx
@@ -1,29 +1,70 @@
 import { useNavigate } from 'react-router-dom';
-import { useAppSelector } from '../../store/hook';
+import { logout } from '../../features/auth/authSlice';
+import { useAppDispatch, useAppSelector } from '../../store/hook';
 
 const Nav = () => {
   const navigate = useNavigate();
-  const { user } = useAppSelector((state) => state.auth.user);
-  console.log(user);
+  const { user } = useAppSelector((state) => state.auth);
+  const dispatch = useAppDispatch();
   return (
-    <div className="w-full h-[60px] flex flex-row p-4">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-6 h-6 text-white ml-auto"
-        onClick={() => {
-          navigate(`/profile/${user.userId}`);
-        }}
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z"
-        />
-      </svg>
+    <div className="w-full h-[60px] flex flex-row p-4 bg-transparent">
+      {user ? (
+        <div className="flex flex-row gap-4 ml-auto">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 text-white"
+            onClick={() => {
+              navigate(`/profile/${user?.user.userId}`);
+            }}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 text-white"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+            onClick={() => {
+              dispatch(logout());
+              navigate('/auth/login');
+            }}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+            />
+          </svg>
+        </div>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 text-white ml-auto"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          onClick={() => {
+            navigate('/auth/login');
+          }}
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+          />
+        </svg>
+      )}
     </div>
   );
 };
