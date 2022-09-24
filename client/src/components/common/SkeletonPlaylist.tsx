62e5ffa5d6dcb1e40cbae87fdd058be9117cb0f0
z00mni <chjm219@kookmin.ac.kr>
2022-09-24 09:23:25 +0000

    rewrite option

--- client/src/components/common/SkeletonPlaylist.tsx
+++ client/src/components/common/SkeletonPlaylist.tsx
@@ -1,10 +1,10 @@
 const SkeletonPlaylist = () => {
   return (
-    <div role="status" className="max-w-sm animate-pulse pl-6">
-      <div className="flex flex-row gap-4">
-        <span className="h-8 bg-gray-700 rounded-md w-48 mb-4"></span>
-        <span className="h-8 bg-gray-700 rounded-md w-48 mb-4"></span>
-        <span className="h-8 bg-gray-700 rounded-md w-48 mb-4"></span>
+    <div role="status" className="w-full max-w-sm animate-pulse px-6">
+      <div className="w-full flex flex-row gap-4">
+        <span className="h-8 bg-gray-700 rounded-md w-16 mb-4"></span>
+        <span className="h-8 bg-gray-700 rounded-md w-16 mb-4"></span>
+        <span className="h-8 bg-gray-700 rounded-md w-16 mb-4"></span>
       </div>
       <span className="sr-only">Loading...</span>
     </div>
