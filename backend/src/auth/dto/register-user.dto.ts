9f22a052cd7586f53de0d2f717a80379e43f9363
z00mni <chjm219@kookmin.ac.kr>
2022-09-25 04:44:47 +0000

    add class-validation (register-user.dto.ts)

--- backend/src/auth/dto/register-user.dto.ts
+++ backend/src/auth/dto/register-user.dto.ts
@@ -1,13 +1,18 @@
-import { IsString, Max } from 'class-validator';
+import { IsString, Max, Min } from 'class-validator';
 
 export class RegisterDto {
   @IsString()
+  @Min(3)
   @Max(8)
   username: string;
 
   @IsString()
+  @Min(7)
+  @Max(12)
   userId: string;
 
   @IsString()
+  @Min(8)
+  @Max(15)
   password: string;
 }
